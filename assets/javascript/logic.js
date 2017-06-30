36465f9d889b7be0ccd1529648aa71cb9a3b294a
Michael Dinh <mkdinh94@gmail.com>
2017-06-29 21:21:19 -0400

    moment js script

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -10,12 +10,17 @@ messagingSenderId: "886032862159"
 
 firebase.initializeApp(config);
 
+
 var name = " ";
 var role = " ";
 var date;
 var rate;
+var monthsWorked;
+var totalBilled;
 
 var database = firebase.database();
+
+updateList();
 
 // Capture Button Click
 $("#submit").on("click", function(event) {
@@ -25,25 +30,35 @@ $("#submit").on("click", function(event) {
     name = $("#name-input").val().trim();
     role = $("#role-input").val().trim();
     date  = $("#date-input").val().trim();
+    console.log(date)
     rate = $("#rate-input").val().trim();
 
     // Code for handling the push
     database.ref().push({
-    name: name,
-    role: role,
-    date: date,
-    rate: rate,
-    dateAdded: firebase.database.ServerValue.TIMESTAMP
+        name: name,
+        role: role,
+        date: date,
+        rate: rate,
+        dateAdded: firebase.database.ServerValue.TIMESTAMP
     });
 
-    $(document).ready(function(){
-        $("#submit").click(function(){
-            event.preventDefault();
-            var monthsWorked ;
-            var totalBilled;
-            var markup = "<tr><td>" + name + "</td><td>" + role + "</td><td>" + date + "</td><td>" + monthsWorked + "</td><td>" + rate + "</td><td>" + totalBilled + "</td></tr>";
-            $("table tbody").append(markup);
-        });
-    });    
-
+    var markup = "<tr><td>" + name + "</td><td>" + role + "</td><td>" + date + "</td><td>" + monthsWorked + "</td><td>" + rate + "</td><td>" + totalBilled + "</td></tr>";
+    $("table tbody").append(markup);
+    updateList()
 });
+
+function updateList(){
+    $(".tbody").empty();
+    firebase.database().ref().on("value", function(snapshot) {
+        snapshot.forEach(function(childSnapshot) {
+            // key
+            var key = childSnapshot.getKey();
+            // value, could be object
+            var childData = childSnapshot.val();
+            monthsWorked = moment().diff(childData.date,"months") ;     
+            totalBilled = parseInt(monthsWorked) * childData.rate;
+            var markup = "<tr><td>" + childData.name + "</td><td>" + childData.role + "</td><td>" + childData.date + "</td><td>" + monthsWorked + "</td><td>" + childData.rate + "</td><td>" + totalBilled + "</td></tr>";
+            $(".tbody").append(markup);
+        });
+    });
+}
